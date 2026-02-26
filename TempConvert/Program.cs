6045c5fb1541f771cf57e7fc5013a474bd17c7ad
ccarreira <cesar.carreira@gmail.com>
2026-02-26 16:12:05 +0000

    first commit

---
+++ TempConvert/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace TempConvert
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+            Console.Write("Temperature: ");
+            float temperature = float.Parse(Console.ReadLine());
+            Console.Write("Unit (C/F): ");
+            string unit = Console.ReadLine();
+        }
+    }
+}
